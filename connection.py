278ba6a1d4f8d731ac2966544fa870d4697157d5
ssbmbeliever <sxl1155@rit.edu>
2016-10-27 19:43:21 -0400

    Delete connection.py
    woot

--- connection.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-from pprint import pprint
-conn = pymysql.connect(host='66.175.208.103',\
-						  port = 3306, user='root',\
-						  passwd='nT2K64bwHB!UR+4JR%P%S?5rYMcZJkw&',
-						  db='Test')
-cursor = conn.cursor()
-cursor.execute("""CALL Test.create_character('{}')""".format("BOb") )
-conn.commit()
