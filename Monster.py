3b35cd4dd71a598789ba07f92414f63b8e9f6c99
thecoltr0n <L3dZ3pp7>
2016-10-27 21:15:20 -0400

    Monster class created, returns monsters fetched

---
+++ Monster.py
@@ -0,0 +1,20 @@
+from Connection import Connection
+
+class Monster():
+    def __init__(self):
+        self.connection = Connection()
+
+    def fetch_monsters(self, dungeon_level):
+        self.connection.cursor.execute("""CALL Test.fetch_monsters('{}')""".format(dungeon_level))
+        self.connection.conn.commit()
+
+        key_list = []
+        for description in self.connection.cursor.description:
+            key_list.append(str(description[0]))
+
+        results = []
+        for data in self.connection.cursor.fetchall():
+            dictionary = dict(zip(key_list,list(data)))
+            results.append(dictionary)
+
+        return results
