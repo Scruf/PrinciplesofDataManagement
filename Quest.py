60393e4e62e1329675b9f4c5adaea384634bc21b
Zach <zsam56@gmail.com>
2016-11-28 08:55:29 -0500

    creating Quest class

---
+++ Quest.py
@@ -0,0 +1,14 @@
+from Connection import Connection
+import json
+from random import randint
+from pprint import pprint
+
+class Quest():
+
+
+
+	def __init__(self):
+		pass
+
+if __name__ == '__main__':
+	quest = Quest()
